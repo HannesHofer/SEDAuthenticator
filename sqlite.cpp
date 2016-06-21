60f23f93953874a5d590d54c89ceeacf65cfb925
Hannes Hofer <Hannes.Hofer@gmail.com>
2016-06-21 21:08:15 +0200

    modify sqlite.cpp to allow initial compilation
    fix compilation errors for initial sqlite.cpp

--- sqlite.cpp
+++ sqlite.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sqlite3.h> 
+#include <sqlite3.h>
+#include <iostream>
+#include <unistd.h>
 
 #define BLOB 0
 #define TEXT 1
@@ -14,43 +16,42 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 	return 0;
 }
 
-int addPreparedStatement(int type, const std::string &value, const std::string name)
+int addPreparedStatement(sqlite3_stmt &stmt, int type, const std::string &value, const std::string &name)
 {
-	int index = sqlite3_bind_parameter_index(stmt, name.c_str());
-    	if (index == 0) {
+	int index = sqlite3_bind_parameter_index(&stmt, name.c_str());
+	if (index == 0) {
 		std::clog << "internal error could not bind parameters" <<  std::endl;
 		exit(-1);
 	}
-
 	switch(type) {
 		case BLOB:
-			return sqlite3_bind_blob(stmt, index, key.c_str(), key.length(), SQLITE_TRANSIENT);
+			return sqlite3_bind_blob(&stmt, index, value.c_str(), value.length(), SQLITE_TRANSIENT);
 			break;
 		case TEXT:
-			return sqlite3_bind_text(stmt, index, key.c_str(), -1, SQLITE_TRANSIENT);
+			return sqlite3_bind_text(&stmt, index, value.c_str(), -1, SQLITE_TRANSIENT);
 			break;
 		default:
 			break;
 	}
 }
 
-int getPassphraseFromKey(const std::string &database, const std::string &key, std::string &passphrase)
+std::string getPassphraseFromKey(const std::string &database, const std::string &key)
 {
-	passphrase = "";
+	std::string passphrase;
 	sqlite3 *db;
 	int rc = sqlite3_open(database.c_str(), &db);
 	if (rc != SQLITE_OK) {
 		std::clog << "could not open database: " << sqlite3_errmsg(db) << std::endl;
-		return -1;  
+		return "";
 	}
 
-	char *query = "SELECT passphrase FROM keymap WHERE key=:key)";
+	const char *query = "SELECT passphrase FROM keymap WHERE key=:key)";
 	
 	sqlite3_stmt *stmt = NULL;
-    	int ret = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+	int ret = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
 	
 	// Prepare & Fill statements
-	addPreparedStatement(BOLB, key, "key");
+	addPreparedStatement(*stmt, BLOB, key, "key");
 	
 	// EXECUTE Statement
 	while ((ret = sqlite3_step(stmt)) == SQLITE_BUSY)
@@ -58,29 +59,32 @@ int getPassphraseFromKey(const std::string &database, const std::string &key, st
 	// TODO get value for query
 
 	if (ret != SQLITE_DONE)
-		std::clog << "return value of step != SQLITE_DONE: " << ret << std::endl;	
+		std::clog << "return value of step != SQLITE_DONE: " << ret << std::endl;
 	
-	return 0;
+	return passphrase;
 }
 
-int addPassphraseKey(const std::string database, const std::string key, const std::string passphrase) 
+int addPassphraseKey(const std::string database, const std::string key, const std::string passphrase)
 {
 	sqlite3 *db;
 	int rc = sqlite3_open(database.c_str(), &db);
 	if (rc != SQLITE_OK) {
 		// CREATE Database when not exists. Warn User
-		std::clog << "could not open " << database << ": " << sqlite3_errmsg(db) << std::endl;
+		std::clog << "could not open " << database << ": "
+		          << sqlite3_errmsg(db) << std::endl;
 		std::clog << "creating new database " << database << std::endl;
-		rc = sqlite3_open_v2(database.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+		rc = sqlite3_open_v2(database.c_str(), &db,
+							 SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
 		if (rc != SQLITE_OK) {
-			std::clog << "could not create " << database << ": " << sqlite3_errmsg(db) << std::endl;
-			exit(-1);	
+			std::clog << "could not create " << database << ": " 
+			          << sqlite3_errmsg(db) << std::endl;
+			exit(-1);
 		}
 		// Create Table 
 		char *zErrMsg = 0;
-		char *sql =  	"CREATE TABLE keymap("
-				"key BLOB NOT NULL,"
-				"passphrase TEXT NOT NULL)";
+		const char *sql =  "CREATE TABLE keymap("
+		                   "key BLOB NOT NULL,"
+				           "passphrase TEXT NOT NULL)";
 		
 		rc = sqlite3_exec(db, sql, NULL, 0, &zErrMsg);
 		if( rc != SQLITE_OK ){
@@ -89,21 +93,21 @@ int addPassphraseKey(const std::string database, const std::string key, const st
 		}
 	}
 
-	char *query = "INSERT OR IGNORE INTO keymap(key, passphrase)"
-                             " VALUES(:key, :passphrase)";
+	const char *query = "INSERT OR IGNORE INTO keymap(key, passphrase)"
+	                    " VALUES(:key, :passphrase)";
 	sqlite3_stmt *stmt = NULL;
-    	int ret = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+	int ret = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
 	
 	// Prepare & Fill statements
-	addPreparedStatement(BOLB, key, "key");
-	addPreparedStatement(BOLB, key, "passphrase");
+	addPreparedStatement(*stmt, BLOB, key, "key");
+	addPreparedStatement(*stmt, TEXT, passphrase, "passphrase");
 	
 	// EXECUTE Statement
 	while ((ret = sqlite3_step(stmt)) == SQLITE_BUSY)
 		usleep(500);
 
 	if (ret != SQLITE_DONE)
-		std::clog << "return value of step != SQLITE_DONE: " << ret << std::endl;	
+		std::clog << "return value of step != SQLITE_DONE: " << ret << std::endl;
 		
 	// delete preared statement
 	ret = sqlite3_finalize(stmt);
@@ -113,36 +117,6 @@ int addPassphraseKey(const std::string database, const std::string key, const st
 
 int main(int argc, char* argv[])
 {
-	sqlite3 *db;
-	char *zErrMsg = 0;
-	int  rc;
-	char *sql;
-
-	/* Open database */
-	rc = sqlite3_open("test.db", &db);
-	if( rc ){
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-		exit(0);
-	}else{
-		fprintf(stdout, "Opened database successfully\n");
-	}
-
-	/* Create SQL statement */
-	sql = "CREATE TABLE COMPANY("  \
-	       "ID INT PRIMARY KEY     NOT NULL," \
-	       "NAME           TEXT    NOT NULL," \
-	       "AGE            INT     NOT NULL," \
-	       "ADDRESS        CHAR(50)," \
-	       "SALARY         REAL );";
-
-	/* Execute SQL statement */
-	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-	if( rc != SQLITE_OK ){
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-	}else{
-		fprintf(stdout, "Table created successfully\n");
-	}
-	sqlite3_close(db);
-	return 0;
+	addPassphraseKey("test.db", "brizzl", "dizzl");
+	std::cout << getPassphraseFromKey("test.db", "brizzl") << std::endl;
 }
